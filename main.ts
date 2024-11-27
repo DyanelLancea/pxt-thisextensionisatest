ea72954573cff783ec1537eb444ea91fb926e847
DyanelLancea <dyanellancea@gmail.com>
2024-11-27 16:02:52 +0800

    Change4

--- main.ts
+++ main.ts
@@ -17,7 +17,7 @@ namespace TelloControl {
 
     function sendCommandToTello(command: string): void {
         // Assuming you're already connected to Tello WiFi, have set up UDP connection and initialisd the Tello into SDK mode
-        sendAT(`AT+CIPSEND=${command.length}`, 500);  // Send command length and command
+        sendAT(`AT+CIPSEND=${command.length}`, 1000);  // Send command length and command
         serial.writeString(command + "\r\n"); // Send the actual command
         basic.pause(500);
         readResponse(); // Display Tello's response
@@ -34,14 +34,14 @@ namespace TelloControl {
     //% rx.defl=SerialPin.P12
     export function initESP8266(tx: SerialPin, rx: SerialPin): void {
         serial.redirect(tx, rx, BaudRate.BaudRate115200); // Redirect TX and RX
-        basic.pause(100);
+        basic.pause(1000);
         serial.setTxBufferSize(128);
         serial.setRxBufferSize(128);
 
         sendAT("AT+RST", 2000); // Reset the ESP8266
-        sendAT("AT+CWMODE=1", 500); // Set ESP8266 to Station Mode (STA mode)
+        sendAT("AT+CWMODE=1", 1000); // Set ESP8266 to Station Mode (STA mode)
         sendAT("AT+CWQAP", 1000); // Disconnect from current Wi-Fi
-        sendAT("AT", 500); // Check if ESP8266 responds with "OK"
+        sendAT("AT", 1000); // Check if ESP8266 responds with "OK"
     }
 
     //% block="Flip"
@@ -83,16 +83,16 @@ namespace TelloControl {
     // Seting up UDP connection (2) and initialise the Tello into SDK mode (3)
     //% block="Initialise ESP and Tello connection"
     export function setupUDPConnection(): void {
-        sendAT(`AT+CIPSTART="UDP","${telloIP}",${commandPort}`, 500);
-        basic.pause(500); // Allow some time for connection setup
+        sendAT(`AT+CIPSTART="UDP","${telloIP}",${commandPort}`, 1000);
+        basic.pause(1000); // Allow some time for connection setup
         sendCommandToTello("command");
-        basic.pause(500); // Allow some time for connection setup
+        basic.pause(1000); // Allow some time for connection setup
     }
 
     // Function to connect to Tello Wi-Fi (1)
     //% block="Connect to Tello Wi-Fi SSID %ssid"
     export function connectToWiFi(ssid: string): void {
-        sendAT(`AT+CWJAP="${ssid}",""`, 5000); // No password is required
+        sendAT(`AT+CWJAP="${ssid}",""`, 8000); // No password is required
         readResponse(); // Display response on micro:bit
     }
 }
