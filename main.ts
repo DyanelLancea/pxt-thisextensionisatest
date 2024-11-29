b1eb540990f1d3d32de2e991e577e35cc3f4070c
DyanelLancea <dyanellancea@gmail.com>
2024-11-29 10:35:51 +0800

    Update Wi-Fi Connection

--- main.ts
+++ main.ts
@@ -1,11 +1,31 @@
 //% color=#126180 icon="\uf0fb" block="ThisExtensionIsATest"
-namespace TelloControl {
-    // Initialize the variables
+    namespace ESP8266_IoT {
+        enum Cmd {
+            None,
+            ConnectWifi,
+        }
+    let wifi_connected: boolean = false
     let telloIP = "192.168.10.1";
     let commandPort = 8889;
     let response = serial.readString();
+    let scanWIFIAPFlag = 0
+    let currentCmd: Cmd = Cmd.None
+    let recvString = ""
 
-    // Function to read and display response on the micro:bit
+        
+    const EspEventSource = 3000
+    const EspEventValue = {
+        None: Cmd.None,
+        ConnectWifi: Cmd.ConnectWifi
+    }
+
+    // write AT command with CR+LF ending
+    function sendAT(command: string, wait: number = 0) {
+        serial.writeString(`${command}\u000D\u000A`)
+        basic.pause(wait)
+    }
+
+// Function to read and display response on the micro:bit
     function readResponse(): void {
         if (response.includes("OK")) {
             basic.showString("Connected");
@@ -14,57 +34,133 @@ namespace TelloControl {
             basic.showString(response); // Display the actual error
         }
     }
-
     function sendCommandToTello(command: string): void {
-        // Assuming you're already connected to Tello WiFi, have set up UDP connection and initialisd the Tello into SDK mode
+         // Assuming you're already connected to Tello WiFi, have set up UDP connection and initialisd the Tello into SDK mode
         sendAT(`AT+CIPSEND=${command.length}`, 500);  // Send command length and command
         serial.writeString(command + "\r\n"); // Send the actual command
         basic.pause(500);
         readResponse(); // Display Tello's response
+        }
+    function restEsp8266() {
+        sendAT("AT+RESTORE", 1000) // restore to factory settings
+        sendAT("AT+RST", 1000) // rest
+        serial.readString()
+        sendAT("AT+CWMODE=1", 500) // set to STA mode
+        sendAT("AT+SYSTIMESTAMP=1634953609130", 100) // Set local timestamp.
+        sendAT(`AT+CIPSNTPCFG=1,8,"ntp1.aliyun.com","0.pool.ntp.org","time.google.com"`, 100)
+        basic.pause(3000)
     }
 
-    function sendAT(command: string, wait: number = 0) {
-        serial.writeString(`${command}\u000D\u000A`);
-        basic.pause(wait);
+    function scanWIFIAP(ssid: string) {
+
+        let scanflag = 0
+        let mscnt = 0
+        recvString = " "
+        sendAT(`AT+CWLAPOPT=1,2,-100,255`)
+        sendAT(`AT+CWLAP`)
+        while (!(scanflag)) {
+
+            recvString = recvString + serial.readString()
+            basic.pause(1)
+            mscnt += 1
+            if (mscnt >= 3000) {
+                scanWIFIAPFlag = 0
+                break
+            }
+
+            if (recvString.includes("+CWLAP:(")) {
+
+                mscnt = 0
+                recvString = recvString.slice(recvString.indexOf("+CWLAP:("))
+                scanflag = 1
+                while (1) {
+
+                    recvString += serial.readString()
+                    basic.pause(1)
+                    mscnt += 1
+
+                    // OLED.clear()
+                    // OLED.writeStringNewLine(_recvString)
+                    if (recvString.includes("OK") || mscnt >= 3000) {
+
+                        if (mscnt >= 3000) {
+                            scanWIFIAPFlag = 0
+                        } else if (recvString.includes(ssid)) {
+                            scanWIFIAPFlag = 1
+                        } else {
+                            scanWIFIAPFlag = 0
+                        }
+                        break
+                    }
+                }
+            }
+
+        }
+        recvString = " "
     }
 
-    // Function to initialize ESP8266 and redirect serial communication
-    //% block="Initialize ESP8266 with TX %tx| RX %rx"
+    /**
+         * Initialize ESP8266 module
+         */
+    //% block="set ESP8266|RX %tx|TX %rx|Baud rate %baudrate"
     //% tx.defl=SerialPin.P8
     //% rx.defl=SerialPin.P12
-    export function initESP8266(tx: SerialPin, rx: SerialPin): void {
-        serial.redirect(tx, rx, BaudRate.BaudRate115200); // Redirect TX and RX
-        basic.pause(100);
-        serial.setTxBufferSize(128);
-        serial.setRxBufferSize(128);
-
-        sendAT("AT+RST", 2000); // Reset the ESP8266
-        sendAT("AT+CWMODE=1", 1000); // Set ESP8266 to Station Mode (STA mode)
-        sendAT("AT+CWQAP", 1000); // Disconnect from current Wi-Fi
-        sendAT("AT", 500); // Check if ESP8266 responds with "OK"
-        readResponse()
-    }
-    //% block="Land"
-    export function land(): void {
-        sendCommandToTello("land");
+    //% ssid.defl=your_ssid
+    //% pw.defl=your_password weight=100
+    export function initWIFI(tx: SerialPin, rx: SerialPin, baudrate: BaudRate) {
+        serial.redirect(tx, rx, BaudRate.BaudRate115200)
+        basic.pause(100)
+        serial.setTxBufferSize(128)
+        serial.setRxBufferSize(128)
+        restEsp8266()
     }
 
-    //% block="Takeoff"
-    export function takeOff(): void {
-        sendCommandToTello("takeoff");
+    /**
+     * connect to Wifi router
+     */
+    //% block="connect Wifi SSID = %ssid|KEY = %pw"
+    //% ssid.defl=your_ssid
+    //% pw.defl=your_pwd weight=95
+    export function connectWifi(ssid: string, pw: string) {
+
+        while (1) {
+            scanWIFIAP(ssid)
+            if (scanWIFIAPFlag) {
+                currentCmd = Cmd.ConnectWifi
+                sendAT(`AT+CWJAP="${ssid}","${pw}"`) // connect to Wifi router
+                control.waitForEvent(EspEventSource, EspEventValue.ConnectWifi)
+                while (!wifi_connected) {
+                    restEsp8266()
+                    sendAT(`AT+CWJAP="${ssid}","${pw}"`)
+                    control.waitForEvent(EspEventSource, EspEventValue.ConnectWifi)
+                }
+                break
+            } else {
+                restEsp8266()
+                currentCmd = Cmd.ConnectWifi
+                sendAT(`AT+CWJAP="${ssid}","${pw}"`)
+                control.waitForEvent(EspEventSource, EspEventValue.ConnectWifi)
+                if (wifi_connected) {
+                    break
+                }
+            }
+        }
     }
+
+    /**
+     * Warning: Deprecated.
+     * Check if ESP8266 successfully connected to Wifi
+     */
+    //% block="Wifi connected %State" weight=70
+    export function wifiState(state: boolean) {
+        return wifi_connected === state
+    }
+
     // Seting up UDP connection (2) and initialise the Tello into SDK mode (3)
     //% block="Initialise ESP and Tello connection"
     export function setupUDPConnection(): void {
         sendAT(`AT+CIPSTART="UDP","${telloIP}",${commandPort}`, 1000);
         sendCommandToTello("command"); //Enter SDK mode
         basic.pause(500); // Allow some time for connection setup
-    }
-
-    // Function to connect to Tello Wi-Fi (1)
-    //% block="Connect to Tello Wi-Fi SSID %ssid Password %pwd"
-    export function connectToWiFi(ssid: string, pwd: string): void {
-        sendAT(`AT+CWJAP="${ssid}","${pwd}"`, 5000);
-        readResponse(); // Display response on micro:bit
-    }
+        }
 }
